11c672f57ddaca37f1b7ce0c67d73419632a13fc
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-02-20 22:17:01 +0200

    removed dynamodb delete protection

--- s3-files/main.tf
+++ s3-files/main.tf
@@ -4,6 +4,7 @@ module "remote_state" {
   override_s3_bucket_name = true
   s3_bucket_name = "jenkins-bucket"
   s3_bucket_name_replica  = "jenkins-bucket-replica"
+  dynamodb_deletion_protection_enabled = false
 
   providers = {
     aws         = aws
