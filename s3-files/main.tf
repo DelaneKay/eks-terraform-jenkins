7294fc9bf7058def7bf77e18950b44e1baa93e24
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-02-20 22:14:50 +0200

    Added a input name for the s3 bucket

--- s3-files/main.tf
+++ s3-files/main.tf
@@ -1,12 +1,12 @@
 module "remote_state" {
   source = "nozaq/remote-state-s3-backend/aws"
 
+  override_s3_bucket_name = true
+  s3_bucket_name = "jenkins-bucket"
+  s3_bucket_name_replica  = "jenkins-bucket-replica"
+
   providers = {
     aws         = aws
     aws.replica = aws.replica
-  }
-
-  tags = {
-    Name = "jenkins-state"
   }
 }
