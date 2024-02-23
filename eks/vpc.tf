09a70453eb9259763875162448ed138d5bbaedc6
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-02-23 17:29:49 +0200

    Added eks vpc

---
+++ eks/vpc.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = "192.168.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["192.168.1.0/24", "192.168.2.0/24"]
+  public_subnets  = ["192.168.4.0/24", "192.168.5.0/24"]
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
+  }
+}
