e3c25f933c153825a554558066976eba1e111ccb
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-03-12 13:54:18 +0200

    Updated code on aws installation code on shell script

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -25,6 +25,7 @@ sudo apt-get update
 sudo apt-get install -y kubectl
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
 
