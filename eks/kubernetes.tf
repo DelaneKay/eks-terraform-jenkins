634f7dfdacfb50759befc1a73edbdaeca74ba164
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-03-16 17:25:20 +0200

    Updated the  kubernetes.tf file

--- eks/kubernetes.tf
+++ eks/kubernetes.tf
@@ -2,5 +2,42 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
   token                  = module.eks.cluster_token
-  # load_config_file       = false # You can uncomment this line if you don't want to load the kubeconfig file
+  # load_config_file       = false  # You can comment this out if you want to explicitly specify the configuration here.
+}
+
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "nginx"
+  }
+}
+
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata[0].name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
 }
